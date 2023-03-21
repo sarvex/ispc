ba9c6a1bfe5ab0e8e4fa521d9e6c9f6e0c73c9f5
Arina Neshlyaeva <arina.neshlyaeva@intel.com>
2023-03-21 15:51:44 -0700

    Update DPC++ API in examples

--- examples/xpu/simple-dpcpp-l0/simple-dpcpp-l0.cpp
+++ examples/xpu/simple-dpcpp-l0/simple-dpcpp-l0.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2022, Intel Corporation
+ * Copyright (c) 2020-2023, Intel Corporation
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -141,7 +141,7 @@ std::vector<float> DpcppApp::transformDpcpp(const std::vector<float> &in) {
 
     // Use accessor to transfer data from the device
     std::vector<float> res(count);
-    const auto out_host_access = out_buffer.get_access<cl::sycl::access::mode::read>();
+    const auto out_host_access = out_buffer.get_host_access();
     for (int i = 0; i < out_host_access.size(); i++) {
         res[i] = out_host_access[i];
     }
