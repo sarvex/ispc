c7dbb78862bb6fe547a2b0230d832cecee6a581f
Arina Neshlyaeva <arina.neshlyaeva@intel.com>
2023-04-14 14:09:04 -0700

    Revert "Experiment with opt.cpp"
    This reverts commit 7ad5786f7faa020fd64147025983e8c899b4ea09.

--- src/opt.cpp
+++ src/opt.cpp
@@ -353,9 +353,7 @@ void ispc::Optimize(llvm::Module *module, int optLevel) {
 #if ISPC_LLVM_VERSION < ISPC_LLVM_15_0
         optPM.add(llvm::createLoopUnswitchPass(false));
 #else
-        if (!g->target->isXeTarget()) {
-            optPM.add(llvm::createSimpleLoopUnswitchLegacyPass(false));
-        }
+        optPM.add(llvm::createSimpleLoopUnswitchLegacyPass(false));
 #endif
         optPM.add(llvm::createInstructionCombiningPass());
         optPM.add(CreateInstructionSimplifyPass());
